fa5478c726b4574bc15d5693509b6df2f69d49e3
Tyson <a1811292@adelaide.edu.au>
2024-09-25 23:53:15 +0930

    Change pivot positions

--- main.py
+++ main.py
@@ -158,10 +158,10 @@ def control_pivots_with_dpad(dynamixel, dpad_inputs, robot_state):
 
     # Prepare positions for sync write
     pivot_positions = {
-        front_pivot_position,
-        rear_pivot_position
+        config['motor_ids']['pivots']['FRONT_PIVOT']: front_pivot_position,
+        config['motor_ids']['pivots']['REAR_PIVOT']: rear_pivot_position
     }
-
+    print("Pivot positions: ", pivot_positions)
     # Sync write the goal positions for the pivots
     dynamixel.set_position_group('Pivot_Group', pivot_positions)
 
