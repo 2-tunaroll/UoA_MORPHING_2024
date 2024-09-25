d9a3307712437d74fed79d485adbea67f0b65553
Tyson <a1811292@adelaide.edu.au>
2024-09-26 00:03:02 +0930

    Testing wheg motors

--- test.py
+++ test.py
@@ -433,6 +433,16 @@ def test_pivot_motors(dynamixel):
     }
     dynamixel.set_position_group('Pivot_Group', pivot_positions)
 
+def test_wheg_motors(dynamixel):
+    dynamixel.torque_on_group('Wheg_Group')
+    dynamixel.set_operating_mode_group('Wheg_Group', 'position')
+    pivot_positions = {
+        7: dynamixel.degrees_to_position(90),
+        8: dynamixel.degrees_to_position(90)
+    }
+    dynamixel.set_position_group('Wheg_Group', pivot_positions)
+
 if __name__ == "__main__":
     dynamixel = DynamixelController()
     test_pivot_motors(dynamixel)
+    test_wheg_motors(dynamixel)
