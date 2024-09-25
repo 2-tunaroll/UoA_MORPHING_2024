355e43ff77a2be07d3c83e62f2ab2fb402f76e4b
Tyson <a1811292@adelaide.edu.au>
2024-09-26 00:20:51 +0930

    fixed another typo

--- main.py
+++ main.py
@@ -154,16 +154,16 @@ def control_pivots_with_dpad(dynamixel, dpad_inputs, robot_state):
 
     # Prepare positions for sync write
     pivot_positions = {
-        config['motor_ids']['pivots']['FRONT_PIVOT']: robot_state.front_pivot_position,
-        config['motor_ids']['pivots']['REAR_PIVOT']: robot_state.rear_pivot_position
+        config['motor_ids']['pivots']['FRONT_PIVOT']: robot_state.front_pivot_angle,
+        config['motor_ids']['pivots']['REAR_PIVOT']: robot_state.rear_pivot_angle
     }
     
     # Sync write the goal positions for the pivots
     dynamixel.set_position_group('Pivot_Group', pivot_positions)
 
     # Logging
-    logging.info(f"Front pivot angle set to {robot_state.front_pivot_angle} degrees (ticks: {robot_state.front_pivot_position})")
-    logging.info(f"Rear pivot angle set to {robot_state.rear_pivot_angle} degrees (ticks: {robot_state.rear_pivot_position})")
+    logging.info(f"Front pivot angle set to {robot_state.front_pivot_angle} degrees (ticks: {robot_state.front_pivot_angle})")
+    logging.info(f"Rear pivot angle set to {robot_state.rear_pivot_angle} degrees (ticks: {robot_state.rear_pivot_angle})")
 
 # Define the initialization for each gait (for whegs only, pivots are disabled)
 def gait_init_1(dynamixel):
