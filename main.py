b7db3372dbdd7898f20d52c4c1722097b04fb846
Tyson <a1811292@adelaide.edu.au>
2024-09-28 14:05:12 +0930

    Changed waiting functionality to allow time for the pivots to move

--- main.py
+++ main.py
@@ -198,11 +198,13 @@ class FLIKRobot:
                     logging.info(f"Front pivot angle set to {self.front_pivot_angle} degrees (ticks: {self.front_pivot_angle})")
                     logging.info(f"Rear pivot angle set to {self.rear_pivot_angle} degrees (ticks: {self.rear_pivot_angle})")
 
-                    await asyncio.sleep(0.25) #Control responsiveness
-            
+                    await asyncio.sleep(0.5) 
+                    
             except Exception as e:
                 logging.error(f"Error controlling pivots: {e}")
-                    
+            
+            await asyncio.sleep(0.25) #Control responsiveness
+        
     # Define the initialization for each gait (for whegs only, pivots are disabled)
     async def gait_init_1(self):
         logging.info("Initialising Gait 1")
