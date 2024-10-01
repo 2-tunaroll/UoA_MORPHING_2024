f1c24e2dc6875f84afd02d3c5064e3611dc2c587
Tyson <a1811292@adelaide.edu.au>
2024-10-01 19:15:32 +0930

    Added wait time to ensure torque gets turned back on

--- main.py
+++ main.py
@@ -239,6 +239,7 @@ class FLIKRobot:
             self.rebbot_requested = False
             self.dynamixel.torque_off_group('All_Motors')
             self.dynamixel.torque_on_group('All_Motors')
+            await asyncio.sleep(1) # Wait one second
         self.wheg_rpm = 0
         self.dynamixel.set_position_group('Wheg_Group', 180)
         self.dynamixel.set_position_group('Pivot_Group', 180)
