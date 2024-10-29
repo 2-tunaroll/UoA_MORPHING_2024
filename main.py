de2f8e2e03feb0563f43c99f15f66c580e1bbc5b
Tyson <a1811292@adelaide.edu.au>
2024-10-29 15:20:53 +1030

    Testy

--- main.py
+++ main.py
@@ -815,7 +815,7 @@ class FLIKRobot:
                 inc_3 = -inc_1
                 inc_4 = -inc_2
                 if self.current_side != "Right":
-                    self.positions = { 1: self.gait4_params['low_pos'], 2: self.gait4_params['high_pos'], 3: self.gait4_params['low_pos'], 4: self.gait4_params['low_pos'], 5: self.gait4_params['high_pos'], 6: self.gait4_params['low_pos'] }
+                    self.positions = { 1: self.gait4_params['low_pos'], 2: self.gait4_params['high_pos'], 3: self.gait4_params['low_pos'], 4: self.gait4_params['high_pos'], 5: self.gait4_params['low_pos'], 6: self.gait4_params['high_pos'] }
                     self.dynamixel.set_position_group('Wheg_Group', self.positions)
                     self.current_side = "Right"
                 time.sleep(0.5)
@@ -826,7 +826,7 @@ class FLIKRobot:
                 inc_1 = -inc_1
                 inc_2 = -inc_2
                 if self.current_side != "Left":
-                    self.positions = { 1: self.gait4_params['low_pos'], 2: self.gait4_params['high_pos'], 3: self.gait4_params['low_pos'], 4: self.gait4_params['low_pos'], 5: self.gait4_params['high_pos'], 6: self.gait4_params['low_pos'] }
+                    self.positions = { 1: self.gait4_params['low_pos'], 2: self.gait4_params['high_pos'], 3: self.gait4_params['low_pos'], 4: self.gait4_params['high_pos'], 5: self.gait4_params['low_pos'], 6: self.gait4_params['high_pos'] }
                     self.dynamixel.set_position_group('Wheg_Group', self.positions)
                     self.current_side = "Left"
             else:
